7c76054e6a2b0e67e2ed9bdde59ac96b039f0261
Paul Richardson <paul.richardson@utah.edu>
2021-06-07 15:58:15 -0600

    assignment 4

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,21 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
+
+    public PetController(final PetService petService) {
+        this.petService = requireNonNull(petService, "PetService is required");
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
